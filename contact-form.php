52d6dbda4ec9cd1c315c20062ba2bf1ca9ea5446
klara-pehe <klarapelhe@outlook.com>
2019-01-31 16:33:15 +0100

    fix contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -3,15 +3,15 @@
     if(isset($_POST['submit'])) {
 
         $name = $_POST['name'];
-        $subject = 'Mail from website';
         $email = $_POST['email'];
-        $message = $_POST['message'];
 
-        $mailTo = 'klarapelhe@outlook.com';
-        $header = 'Mail from '. $email;
-        $txt = 'You have received an email from '. $name;
-        
-        mail($mailTo, $subject, $txt, $header);
+        $mailTo = 'info@klarapelhe.rs';
+        $subject = 'Novu poruku salje ' . $name;
+        $message = $_POST['message'];
+        $headers = "Poruku vam salje " . $name . " sa sledece email adrese: " . $email;
+
+        mail($mailTo, $subject, $message, $headers);
+        header('Location: ./index.html'); 
     }
 
 ?>
